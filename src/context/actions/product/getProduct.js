c5d1e65035a8671d69ae9338539d08c49417d693
ojha007 <ojhaamir007@gmail.com>
2020-10-04 23:52:31 +0545

    fixes base url

--- src/context/actions/product/getProduct.js
+++ src/context/actions/product/getProduct.js
@@ -1,28 +1,29 @@
 import axios from "axios";
 import {
-  PRODUCT_LOADING,
-  PRODUCT_LOAD_SUCCESS,
-  PRODUCT_LOAD_ERROR,
+    PRODUCT_LOADING,
+    PRODUCT_LOAD_SUCCESS,
+    PRODUCT_LOAD_ERROR,
 } from "../../../constants/actionTypes";
+
 export default () => (dispatch) => {
-  dispatch({
-    type: PRODUCT_LOADING,
-  });
-  axios
-    .get(
-      "https://laxmipujapasal.tk/api/v2/dashboard?fbclid=IwAR3vhrE0nEnWrvnpF_PM5uxEQOR9wgSn-aIddDVtZZup4iweQJXXwCzP0Jw"
-    )
-    .then((res) => {
-      dispatch({
-        type: PRODUCT_LOAD_SUCCESS,
-        payload: res.data.data,
-      });
-    })
-    .catch((err) => {
-      console.log(err.message);
-      dispatch({
-        type: PRODUCT_LOAD_ERROR,
-        payload: err.message,
-      });
+    dispatch({
+        type: PRODUCT_LOADING,
     });
+    axios
+        .get(
+            "https://laxmipujapasal.tk/api/v2/dashboard"
+        )
+        .then((res) => {
+            dispatch({
+                type: PRODUCT_LOAD_SUCCESS,
+                payload: res.data.data,
+            });
+        })
+        .catch((err) => {
+            console.log(err.message);
+            dispatch({
+                type: PRODUCT_LOAD_ERROR,
+                payload: err.message,
+            });
+        });
 };
