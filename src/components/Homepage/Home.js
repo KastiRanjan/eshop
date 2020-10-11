b38cd022e332663c6e64c58749b2c6198e0e8f60
Ranjan <Rkastew@gmail.com>
2020-10-12 00:00:47 +0545

    category

--- src/components/Homepage/Home.js
+++ src/components/Homepage/Home.js
@@ -10,32 +10,24 @@ import Slider from "./Slider/Slider";
 import { css } from "@emotion/core";
 import ClipLoader from "react-spinners/ClipLoader";
 
-
 const override = css`
   display: block;
   margin: 0 auto;
   border-color: #f57224;
-  height:25px;
-  width:40px
+  height: 25px;
+  width: 40px;
 `;
 export default function Home() {
-  const { productState,} = useContext(GlobalContext);
+  const { productState } = useContext(GlobalContext);
   const { loading } = productState;
-
 
   if (loading) {
     return (
-     <div className="container flex flex-jc-c" style={{height:"450px",position:"relative"}}>
-      <div className="loader">
-      <ClipLoader
-              css={override}
-              size={150}
-              color={"#123abc"}
-              loading={loading}
-            /> <br/>
-      
+      <div className="container flex flex-jc-c" style={{ height: "450px", position: "relative" }}>
+        <div className="loader">
+          <ClipLoader css={override} size={150} color={"#123abc"} loading={loading} /> <br />
+        </div>
       </div>
-     </div>
     );
   }
   return (
@@ -43,7 +35,7 @@ export default function Home() {
       <Slider />
       <Collection />
       <OurProudct />
-      <HotDeal />
+      {/* <HotDeal /> */}
       <NewArrival />
       <MostPopular />
     </div>
